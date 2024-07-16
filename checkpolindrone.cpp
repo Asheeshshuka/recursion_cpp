fe20788be62e337d44ca5036b88ba6f757f6a370
ASHEESH. <139006643+Asheeshshuka@users.noreply.github.com>
2024-07-16 20:31:18 +0530

    Update checkpolindrone.cpp

--- checkpolindrone.cpp
+++ checkpolindrone.cpp
@@ -16,3 +16,22 @@ int main(){
 string str=" naman";
 cout<<checkpal(str,0,4)<<endl;
 }
+//iterative way using 
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a number"<<endl;
+        cin>>n;
+    int rev=0;
+    while(n!=0){
+        int last=n%10;
+        rev=rev*10+last;
+        n=n/10;
+        }
+        if(n==rev)
+       cout<< "polindrone"<<endl;
+       else
+       cout<<"not polindrone"<<endl;
+        return 0;
+}
